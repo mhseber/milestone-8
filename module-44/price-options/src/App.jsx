cc8cdbaba9ccc8219ac6772cbea58fd41847460e
mahamudul hasan seber <sebermh@gmail.com>
2024-10-28 16:58:09 +0600

    module-44

---
+++ module-44/price-options/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import DaisyNav from './components/DaisyUi/DaisyNav'
+import NavBar from './components/DaisyUi/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-950 text-yellow-200 text-3xl text-center p-10'>MH SEBER</h1>
+      <DaisyNav></DaisyNav>
+      <NavBar></NavBar>
+
+
+
+    </>
+  )
+}
+
+export default App
